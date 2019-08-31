1be3e12f6044c70fab6389e03a6ef96c3f38b9b1
asd815909 <1124397469@qq.com>
2019-09-01 03:15:46 +0800

    第一次提交

---
+++ mybook-server/src/main/java/com/cjc/mybook/MybookServerApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.mybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybookServerApplication.class, args);
+    }
+
+}
